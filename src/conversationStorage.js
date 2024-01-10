db3f247c82376b7661af0af7f670829a6dda0c79
Fearg <ferskav@gmail.com>
2024-01-10 23:02:52 +0000

    Added file to store chat conversation array

---
+++ src/conversationStorage.js
@@ -0,0 +1,9 @@
+// conversationStorage.js
+const conversationHistory = [];
+
+modules.exports = {
+    getConversation: () => [...converation],
+    addConversation: (conversation) => {
+        conversationHistory.push(conversation);
+    },
+};
